35fa91d41af10c2e26c29c9701c4b23a89ae343c
leegeunhyeok <lghlove0509@naver.com>
2019-11-03 17:21:26 +0900

    UPDATE: Parser

--- src/parser.py
+++ src/parser.py
@@ -27,6 +27,8 @@ from multiprocessing import current_process
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
+from src.util import extract_date, to_valid_filename
+
 class Parser:
     def __init__(self, chromedriver, cookie, logger, delay):
         self._chromedriver = chromedriver
@@ -48,10 +50,32 @@ class Parser:
         while(feeder_running.value):
             try:
                 if len(content_list) != 0:
+                    # 공유 리스트에서 게시물 URL 추출 및 접속
                     target_url = content_list.pop()
                     self._logger.info(current_process().name, target_url)
                     parser_driver.get(target_url)
+
+                    # 필요한 데이터 추출
+                    date = parser_driver.find_element_by_css_selector('div.view1 p')
+                    images = parser_driver.find_elements_by_css_selector('section.imageBox')
+                    title = parser_driver.find_element_by_id('cyco-post-title')
+                    post_date = extract_date(date.get_attribute('innerText'))
+
+                    title = to_valid_filename(title.get_attribute('innerText'))
+
+                    # 이미지 목록 추출
+                    for image in images:
+                        imgs = image.find_elements_by_tag_name('img')
+
+                        for img in imgs:
+                            image_list.append({
+                                'title': title,
+                                'date': post_date,
+                                'src': img.get_attribute('src')
+                            })
+                            self._logger.info('{}_{} 포스트 파싱 됨'.format(post_date, title))
                 else:
+                    # 만약 리스트가 비어있는 경우 1초 대기 후 다시 시도
                     time.sleep(1)
             except Exception as e:
                 self._logger.error(e)
